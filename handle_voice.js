38a33046e412ed3931d35ebc666c233ece87991e
Arjuna-Dev <arjuna1@protonmail.com>
2024-04-14 16:47:13 +0200

    Fixed image appearing logic

---
+++ handle_voice.js
@@ -0,0 +1,64 @@
+let contentImage = document.getElementById("contentImage");
+let contentImage1 = document.getElementById("contentImage1");
+let contentImage2 = document.getElementById("contentImage2");
+let contentImage3 = document.getElementById("contentImage3");
+let contentImage4 = document.getElementById("contentImage4");
+
+export function handleVoice(command) {
+  let contentText = document.getElementById("contentText");
+  contentText.style.display = "block";
+
+  let contentTitle = document.getElementById("contentTitle");
+  let contentBody = document.getElementById("contentBody");
+
+  switch (command) {
+    case "home":
+      contentTitle.textContent = "Unlock the Natural Way of Language Learning";
+      contentBody.innerHTML = `
+            At parakeet, we revolutionize language learning by mimicking the natural learning process.
+            <ul>
+                <li>Learn in the same way you became a native speaker of your mother tongue.</li>
+                <li>Experience the power of learning a language organically through engaging, AI-generated scenarios tailored to your interests and needs.</li>
+                <li>Forget about screens, tedious grammar drills, and endless repetition without context.</li>
+                <li>Immerse yourself in practical, everyday conversations, just like a native speaker would.</li>
+            </ul>
+        `;
+      contentImage1.classList.remove("hidden");
+      contentImage2.classList.add("hidden");
+      contentImage3.classList.add("hidden");
+      contentImage4.classList.add("hidden");
+      contentImage.style.display = "none";
+      break;
+    case "waiting list":
+      contentTitle.textContent = "Get Early Access";
+      contentBody.textContent = "Ready to change how you learn languages? Join our waiting list";
+      contentImage2.classList.remove("hidden");
+      contentImage1.classList.add("hidden");
+      contentImage3.classList.add("hidden");
+      contentImage4.classList.add("hidden");
+      contentImage.style.display = "none";
+      break;
+    case "contact":
+      contentTitle.textContent = "Connect With Us";
+      contentBody.textContent = "Have questions or want to know more? Our team is just a message away. Contact us via [contact@email.com]";
+      contentImage3.classList.remove("hidden");
+      contentImage1.classList.add("hidden");
+      contentImage2.classList.add("hidden");
+      contentImage4.classList.add("hidden");
+      contentImage.style.display = "none";
+      break;
+    case "who we are":
+      contentTitle.textContent = "Our Story";
+      contentBody.textContent =
+        "We are technologists, and avid learners who believe in a world where language learning is as natural and engaging as conversation itself. Founded on the principles of cognitive science and AI, parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.";
+      contentImage4.classList.remove("hidden");
+      contentImage1.classList.add("hidden");
+      contentImage2.classList.add("hidden");
+      contentImage3.classList.add("hidden");
+      contentImage.style.display = "none";
+      break;
+    default:
+      console.log("Command not recognized");
+      break;
+  }
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { drawAudioVisual } from "./draw_audio_visual.js";
+import { handleVoice } from "./handle_voice.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Check if the browser supports the Web Speech API
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var last = event.results.length - 1;
     var command = event.results[last][0].transcript.trim().toLowerCase();
     console.log("Voice command recognized:", command);
-    navigateToContent(command);
+    handleVoice(command);
   };
 
   recognition.onstart = function () {
@@ -34,66 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
     closeModal();
     handleStartRecognition();
   });
-
-  function navigateToContent(command) {
-    const contentDiv = document.querySelector(".content");
-
-    let contentImage = document.getElementById("contentImage");
-    contentImage.style.display = "none";
-
-    let contentText = document.getElementById("contentText");
-    contentText.style.display = "block";
-
-    let contentTitle = document.getElementById("contentTitle");
-    let contentBody = document.getElementById("contentBody");
-
-    switch (command) {
-      case "home":
-        contentTitle.textContent = "Unlock the Natural Way of Language Learning";
-        contentBody.innerHTML = `
-            At parakeet, we revolutionize language learning by mimicking the natural learning process.
-            <ul>
-                <li>Learn in the same way you became a native speaker of your mother tongue.</li>
-                <li>Experience the power of learning a language organically through engaging, AI-generated scenarios tailored to your interests and needs.</li>
-                <li>Forget about screens, tedious grammar drills, and endless repetition without context.</li>
-                <li>Immerse yourself in practical, everyday conversations, just like a native speaker would.</li>
-            </ul>
-        `;
-        contentImage1.classList.remove("hidden");
-        contentImage2.classList.add("hidden");
-        contentImage3.classList.add("hidden");
-        contentImage4.classList.add("hidden");
-        break;
-      case "waiting list":
-        contentTitle.textContent = "Get Early Access";
-        contentBody.textContent = "Ready to change how you learn languages? Join our waiting list";
-        contentImage2.classList.remove("hidden");
-        contentImage1.classList.add("hidden");
-        contentImage3.classList.add("hidden");
-        contentImage4.classList.add("hidden");
-        break;
-      case "contact":
-        contentTitle.textContent = "Connect With Us";
-        contentBody.textContent = "Have questions or want to know more? Our team is just a message away. Contact us via [contact@email.com]";
-        contentImage3.classList.remove("hidden");
-        contentImage1.classList.add("hidden");
-        contentImage2.classList.add("hidden");
-        contentImage4.classList.add("hidden");
-        break;
-      case "who we are":
-        contentTitle.textContent = "Our Story";
-        contentBody.textContent =
-          "We are technologists, and avid learners who believe in a world where language learning is as natural and engaging as conversation itself. Founded on the principles of cognitive science and AI, parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.";
-        contentImage4.classList.remove("hidden");
-        contentImage1.classList.add("hidden");
-        contentImage2.classList.add("hidden");
-        contentImage3.classList.add("hidden");
-        break;
-      default:
-        console.log("Command not recognized");
-        break;
-    }
-  }
 
   function closeModal() {
     var modal = document.getElementById("voiceModal");
