3c850f832568e22b14079f1ae166e0c3a90149f2
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-04-17 00:12:55 +0200

    Update Content

--- handle_voice.js
+++ handle_voice.js
@@ -47,16 +47,20 @@ export function handleVoice(command) {
 
   switch (command) {
     case "home":
-      contentTitle.textContent = "Unlock the Natural Way of Language Learning";
-      contentBody.innerHTML = `
-            At parakeet, we revolutionize language learning by mimicking the natural learning process.
-            <ul>
-                <li>Learn in the same way you became a native speaker of your mother tongue.</li>
-                <li>Experience the power of learning a language organically through engaging, AI-generated scenarios tailored to your interests and needs.</li>
-                <li>Forget about screens, tedious grammar drills, and endless repetition without context.</li>
-                <li>Immerse yourself in practical, everyday conversations, just like a native speaker would.</li>
-            </ul>
-            `;
+      contentBody.innerHTML = 
+      `
+        <div>
+          <div>No screens, No grammar drills, No rote memorization & No endless repetition without context</div>
+
+          <div>
+            Learn Languages Like You Learned Your First
+          </div>
+      
+          <div>Immerse yourself in practical, everyday conversations, just like a native speaker would.</div>
+      
+          <div>Parakeet uses advanced AI to simulate real-life conversations, so you learn to speak by actually speaking.</div>
+        </div>
+      `;
       homeButton.classList.add("active");
       waitingListButton.classList.remove("active");
       contactButton.classList.remove("active");
@@ -68,17 +72,26 @@ export function handleVoice(command) {
       contentImage4.classList.add("hidden");
       contentImage.style.display = "none";
       break;
+    
     case "waiting list":
-      contentTitle.textContent = "Get Early Access";
-      contentBody.innerHTML = `
-      Ready to change how you learn languages? Join our waiting list
-        <br>
-        <form id = "subscriptionForm" action="https://docs.google.com/forms/u/5/d/e/1FAIpQLSdDx3ou_lS9Q7VIXvBfXF1qEmU4SUlPpdKVWaksdy3_QTmn7g/formResponse">
-          <label for="email">Your email:</label><br>
-          <input type="email" id="emailSubscription" name="emailAddress" required><br>
-          <input type="submit" value="Subscribe">
-        </form>
+      contentBody.innerHTML = 
+      `
+        <div>
+          <div>Parakeet creates personalized learning experiences that adapt to your pace and style using advanced AI.</div>
+
+          <div>Subscribers gain early access to our beta, special launch promotions, and insights into our AI-driven approach.</div>
+
+          <div>Join Our Exclusive Waiting List</div>
+      
+          <br>
+          <form id = "subscriptionForm" action="https://docs.google.com/forms/u/5/d/e/1FAIpQLSdDx3ou_lS9Q7VIXvBfXF1qEmU4SUlPpdKVWaksdy3_QTmn7g/formResponse">
+            <label for="email">Your email:</label><br>
+            <input type="email" id="emailSubscription" name="emailAddress" required><br>
+            <input type="submit" value="Subscribe">
+          </form>
+        </div>
       `;
+      
       document.getElementById("subscriptionForm").addEventListener("submit", function (e) {
         e.preventDefault(); // Stop the form from submitting the traditional way
       
@@ -110,10 +123,18 @@ export function handleVoice(command) {
       contentImage4.classList.add("hidden");
       contentImage.style.display = "none";
       break;
+    
     case "contact":
-      contentTitle.textContent = "Connect With Us";
-      contentBody.innerHTML = `
-      Have questions or want to know more? Our team is just a message away. Contact us via the form
+      contentBody.innerHTML = 
+      `
+        <div>Connect With Us</div>
+
+        <div>Have questions, suggestions or want to know more?</div>
+        
+        <div>Or just want to chat about your language journey, we’re all ears.</div>
+
+        <div>Drop us a message anytime.</div>
+
         <br>
         <form id = "contactForm" action="https://docs.google.com/forms/u/5/d/182IvO9Cf05t-krEHsBKH9eBuM7D5ZX9b_qsNi4NhC1M/formResponse">
           <label for="email">Your email:</label><br>
@@ -123,6 +144,7 @@ export function handleVoice(command) {
           <input class="form-element" type="submit" value="Send message">
         </form>
       `;
+      
       document.getElementById("contactForm").addEventListener("submit", function (e) {
         e.preventDefault(); // Stop the form from submitting the traditional way
       
@@ -154,29 +176,56 @@ export function handleVoice(command) {
       contentImage4.classList.add("hidden");
       contentImage.style.display = "none";
       break;
+    
     case "who we are":
-      contentTitle.textContent = "Our Story";
-      contentBody.innerHTML = `We are technologists, and avid learners who believe in a world where language learning is as natural and engaging as conversation itself. Founded on the principles of cognitive science and AI, parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.
+      contentBody.innerHTML = 
+      `
+        <div>Our mission is to recreate the natural way of learning languages through advanced AI.</div>
+
+        <div>We believe in empowering people to speak any language confidently.</div>
+
+        <div>Founded on the principles of cognitive science and AI, Parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.</div>
+
         <div class="founders">
           <div class="founder">
             <img src="img/founders/alejh.png" alt="Founder 1" class="founder-image">
-            <a href = "https://www.linkedin.com/in/alejandrocamus/" target = "_blank"> <p class="founder-name">Alejandro</p> </a>
+            <div class="founder-name">Alejandro</div>
+            <div class="languages-known">Languages I know: Spanish, English, some German</div>
+            <div class="languages-wishlist">Wants to learn: German (C1), Tamil, Bengali</div>
+            <a href = "https://www.linkedin.com/in/alejandrocamus/" target = "_blank"> 
+              <img src="img/linkedin.png" alt="Linkedin">
+            </a>
           </div>
           <div class="founder">
             <img src="img/founders/mukesh.jpeg" alt="Founder 2" class="founder-image">
-            <a href = "https://www.linkedin.com/in/mukesh-jaiswal-23b5b0a9/" target = "_blank"><p class="founder-name">Mukesh</p></a>
+            <div class="founder-name">Mukesh</div>
+            <div class="languages-known">Languages I know: Nepali, Hindi, Bhojpuri, English</div>
+            <div class="languages-wishlist">Wants to learn: German, Russian, Spanish</div>
+            <a href = "https://www.linkedin.com/in/mukesh-jaiswal-23b5b0a9/" target = "_blank">
+              <img src="img/linkedin.png" alt="Linkedin">
+            </a>
           </div>
           <div class="founder">
             <img src="img/founders/burcu.jpeg" alt="Founder 3" class="founder-image">
-            <a href = "https://www.linkedin.com/in/burcu-baycan-900926176/" target = "_blank"><p class="founder-name">Burcu</p></a>
+            <div class="founder-name">Burcu</div>
+            <div class="languages-known">Languages I know: Turkish, English</div>
+            <div class="languages-wishlist">Wants to learn: German, Spanish</div>
+            <a href = "https://www.linkedin.com/in/burcu-baycan-900926176/" target = "_blank">
+              <img src="img/linkedin.png" alt="Linkedin">
+            </a>
           </div>
           <div class="founder">
             <img src="img/founders/aman.png" alt="Founder 4" class="founder-image">
-            <a href = "https://www.linkedin.com/in/aman-jaiswal-5b3354178/" target = "_blank"><p class="founder-name">Aman</p></a>
+            <div class="founder-name">Aman</div>
+            <div class="languages-known">Languages I know: Nepali, Hindi, Bhojpuri, English</div>
+            <div class="languages-wishlist">Wants to learn: German, Spanish</div>
+            <a href = "https://www.linkedin.com/in/aman-jaiswal-5b3354178/" target = "_blank">
+              <img src="img/linkedin.png" alt="Linkedin">
+            </a>
           </div>
         </div>
         
-        `;
+      `;
 
       homeButton.classList.remove("active");
       waitingListButton.classList.remove("active");
