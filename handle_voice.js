0a8c72da4de7d69fc1bcb1ba012f177acf8e11d8
Arjuna-Dev <arjuna1@protonmail.com>
2024-04-17 11:51:01 +0200

    Design improvements

--- handle_voice.js
+++ handle_voice.js
@@ -31,8 +31,8 @@ document.getElementById("whoWeAreButton").addEventListener("click", function () 
 });
 
 export function handleVoice(command) {
-  let contentText = document.getElementById("contentText");
-  contentText.style.display = "block";
+  let contentMain = document.getElementById("contentMain");
+  contentMain.style.display = "block";
 
   let contentTitle = document.getElementById("contentTitle");
   let contentBody = document.getElementById("contentBody");
@@ -49,11 +49,11 @@ export function handleVoice(command) {
     case "home":
       contentTitle.textContent = "Unlock the Natural Way of Language Learning";
       contentBody.innerHTML = `
-      <div class="content-container">
+      <div class="content-container-home">
         <p>Learn languages like you learned your first.</p>
         <p><span class="bold">No screens, no grammar drills, no rote memorization,</span> and <span class="bold">no endless repetition without context</span>.</p>
         <p>Immerse yourself in practical, everyday conversations, just like a native speaker would.</p>
-        <p>Parakeet uses advanced AI to simulate real-life conversations, so you learn to speak by actually speaking.</p>
+        <p>parakeet uses GenAI to create real-life conversations so you learn to speak by actually speaking.</p>
       </div>
       `;
       homeButton.classList.add("active");
@@ -72,13 +72,13 @@ export function handleVoice(command) {
       contentTitle.textContent = "Get Early Access";
       contentBody.innerHTML = `
         <div class="content-container">
-          <p>Parakeet creates personalized learning experiences that adapt to your pace and style using advanced AI.</p>
+          <p>Parakeet creates personalized learning experiences that adapt to your pace and style using GenAI.</p>
 
-          <p>Subscribers gain early access to our beta, special launch promotions, and insights into our AI-driven approach.</p>
+          <p>Subscribers gain early access to our beta, special launch promotions, and insights into our approach.</p>
 
-          <p>Join Our Exclusive Waiting List</p>
+          <p>Join our waiting list.</p>
 
-          <p>Ready to change how you learn languages? Join our waiting list</p>
+          <p>Ready to change how you learn languages? Join our waiting list.</p>
         </div>
         <div>
           <br>
@@ -183,13 +183,13 @@ export function handleVoice(command) {
 
         <p>We believe in empowering people to speak any language confidently.</p>
 
-        <p>Founded on the principles of cognitive science and generative AI, Parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.</p>
+        <p>Founded on the principles of cognitive science and generative AI, parakeet is designed to make you fluent by leveraging the natural methods that helped you learn your mother tongue.</p>
 
         <div class="founders">
           <div class="founder">
             <img src="img/founders/alejh.png" alt="Founder 1" class="founder-image">
             <div class="founder-name">Alejandro</div>
-            <div class="languages-known">Languages I know: Spanish, English, some German</div>
+            <div class="languages-known">Knows: Spanish, English, some German</div>
             <div class="languages-wishlist">Wants to learn: German (C1), Tamil, Bengali</div>
             <a href = "https://www.linkedin.com/in/alejandrocamus/" target = "_blank"> 
               <svg class="linkedin-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"/></svg>
